cdba49685e453d7e4136ae30b11ddc9f9d234b6f
is0695 <123656972+is0695@users.noreply.github.com>
2023-11-18 19:14:44 +0300

    Add files via upload

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 
-from .models import Article
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Relationship, Tag
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            form.cleaned_data
+            if len(self.forms) == 0:
+                raise ValidationError('Не указаны теги!')
+
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Relationship
+    formset = RelationshipInlineFormset
+    extra = 0
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title']
+    inlines = [RelationshipInline]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,25 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=256, verbose_name='Название')
+    articles = models.ManyToManyField(Article, related_name='articletag', through='Relationship')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+class Relationship(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, verbose_name='Раздел', on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(verbose_name='Основной')
